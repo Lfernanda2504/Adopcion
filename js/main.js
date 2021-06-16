d35b6f220e2641bc0f631efcab2b112146bd981a
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-06-15 23:38:29 -0500

    actualización archivos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-let 
+
+const form = document.querySelector('form').addEventListener("submit", e =>{
+const nameInput = document.querySelector('name').value;
+const lastnameInput = document.querySelector('lastname').value;
+const emailInput = document.querySelector('email').value;
+const submitBtn = document.querySelector('enviar').value;
+
+
+})
+console.log(nameInput)
+
+
+
+
+
