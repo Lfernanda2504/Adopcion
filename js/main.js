dc13bc1319fd62bda4aa78d1c3abfd0c4f683997
lfernanda2504 <luizafernandagarcia@gmail.com>
2021-06-16 19:49:09 -0500

    insert views

--- js/main.js
+++ js/main.js
@@ -1,15 +1,2 @@
-
-const form = document.querySelector('form').addEventListener("submit", e =>{
-const nameInput = document.querySelector('name').value;
-const lastnameInput = document.querySelector('lastname').value;
-const emailInput = document.querySelector('email').value;
-const submitBtn = document.querySelector('enviar').value;
-
-
-})
-console.log(nameInput)
-
-
-
-
+var nameInput = document.getElementById('name');
 
